910712fd1e441622e2737f2f119816ceaec705af
Hakan Cevirgen <hakancevirgen@gmail.com>
2021-03-02 13:30:48 +0300

    add: admin panel views

--- scheduler/rest_requests/admin.py
+++ scheduler/rest_requests/admin.py
@@ -1,12 +1,76 @@
 from django.contrib import admin
 from scheduler.rest_requests.models import RestRequest, RequestSchedule
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 @admin.register(RequestSchedule)
 class RequestScheduleAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "rest_request",
+        "scheduled_date_time",
+        "status",
+    )
+    fields = (
+        "rest_request",
+        "scheduled_date_time",
+        "status",
+        "response_data",
+    )
+    readonly_fields = ("response_data",)
+    list_filter = ("status", "rest_request__method")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+
+        if not obj:
+            form.base_fields["rest_request"].queryset = RestRequest.objects.filter(
+                user=request.user
+            )
+        return form
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ["rest_request", "response_data"]
+        else:
+            return ["response_data"]
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.user = request.user
+        obj.save()
 
 
 @admin.register(RestRequest)
 class RestRequestAdmin(admin.ModelAdmin):
-    pass
+    fields = (
+        "request_name",
+        "url",
+        "method",
+        "headers",
+        "body",
+    )
+    list_display = (
+        "method",
+        "url",
+        "request_name",
+    )
+    list_filter = ("method",)
+
+    def get_queryset(self, request):
+        qs = super(RestRequestAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(user=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.user = request.user
+        obj.save()
