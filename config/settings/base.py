44af0bf6b945a335084ac8e44005d14b19d72c8c
Hakan Cevirgen <hakancevirgen@gmail.com>
2021-03-02 23:20:51 +0300

    change: redirect url

--- config/settings/base.py
+++ config/settings/base.py
@@ -181,7 +181,7 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "/admin"
+LOGIN_REDIRECT_URL = "/panel"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
 
